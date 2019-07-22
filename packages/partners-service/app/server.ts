130a9919a7b9786e9d26e012a35ca7f33b895b97
Hieu Tran <hieutran.fu@gmail.com>
2019-07-22 12:01:11 +0700

    Update tsconfig

--- packages/partners-service/app/server.ts
+++ packages/partners-service/app/server.ts
@@ -1,10 +1,10 @@
-import * as Koa from 'koa';
+import Koa from 'koa';
 
 const app = new Koa();
 app.use(async ctx => {
-    ctx.body = 'Hello from partners-service';
+  ctx.body = 'Hello from partners-service';
 });
 
 console.log(`Partners service is running on port 3000`);
-  
+
 app.listen(3000);
